f1af6d64da6a32a38477dd61adf62bde0bf4fe4e
Alberto Miorin <32617+amiorin@users.noreply.github.com>
2026-02-26 17:36:08 +0100

    fix: ::end

--- src/io/github/amiorin/alice/tools.clj
+++ src/io/github/amiorin/alice/tools.clj
@@ -9,8 +9,8 @@
    [cheshire.core :as json]))
 
 (def step-fns [workflow/print-step-fn
-               (step-fns/->exit-step-fn ::end)
-               (step-fns/->print-error-step-fn ::end)])
+               (step-fns/->exit-step-fn ::workflow/end)
+               (step-fns/->print-error-step-fn ::workflow/end)])
 
 (defn tofu
   [step-fns opts]
